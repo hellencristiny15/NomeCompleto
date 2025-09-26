deacf453521018e390ca3ccc1bc7a9bebd48b7b6
Hellen <cristinyhellen75@gmail.com>
2025-09-26 20:06:29 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("PRIMEIRO NOME :");
+string nome = Console.ReadLine();
+Console.WriteLine("SOBRENOME :");
+string sobrenome = Console.ReadLine();
+Console.WriteLine($"NOME COMPLETO : {nome} {sobrenome}");
+Console.WriteLine($"NOME COMPLETO EM MAIUSCULO : {nome.ToUpper()} {sobrenome.ToUpper()}");
+Console.WriteLine($"NOME COMPLETO EM MINUSCULO : {nome.ToLower()} {sobrenome.ToLower()}");
